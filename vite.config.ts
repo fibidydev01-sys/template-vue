af4d8a9c273b618a7dac87de8d20914596e827cb
Fibidy-Developer <fibidydev01@gmail.com>
2025-12-29 10:10:19 +0700

    final-commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(), // Tailwind v4 via Vite plugin
+    tailwindcss(),
   ],
   resolve: {
     alias: {
